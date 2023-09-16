b3b595581e34928d168f1859cca5cf1ff3f1590c
Ibbocs <99998326+Ibbocs@users.noreply.github.com>
2023-09-16 11:18:11 +0400

    GlobalExceptionMidillewavre And Program.cs

---
+++ DormitoryAPI.Presentation/Middlewares/GlobalExceptionHandlerMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Mime;
+using System.Net;
+using System.Text.Json;
+
+namespace DormitoryAPI.Presentation.Middlewares
+{
+    public class GlobalExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<GlobalExceptionHandlerMiddleware> _logger;
+
+        public GlobalExceptionHandlerMiddleware(RequestDelegate next, ILogger<GlobalExceptionHandlerMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                //http context xeta kodun veririk 500>
+                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+                //content type                  mime fw daha guvenli 
+                context.Response.ContentType = MediaTypeNames.Application.Json;
+
+                _logger.LogError($"-- Status Code: {context.Response.StatusCode} - Source: {ex.Source} - Message: {ex.Message} - Inner: {ex.InnerException} --");
+                var errorResponse = new
+                {
+                    StatusCode = context.Response.StatusCode,
+                    Message = ex.Message,
+                    Title = "Internal Error"
+                };
+                var errorResponseJson = JsonSerializer.Serialize(errorResponse);
+                await context.Response.WriteAsync(errorResponseJson);
+
+            }
+        }
+    }
+}
--- DormitoryApi.Presentation/Program.cs
+++ DormitoryApi.Presentation/Program.cs
@@ -1,12 +1,22 @@
 using DormitoryApi.Application.AutoMapper;
+using DormitoryApi.Application.Validation.UserValid;
 using DormitoryApi.Infrastructure.Extentions;
 using DormitoryApi.Persistance.Extentions;
+using DormitoryAPI.Presentation.Middlewares;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.HttpLogging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Security.Claims;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddFluentValidation(config => config.RegisterValidatorsFromAssemblyContaining<CreatUserValidator>());
 
 //Persistance Layer Services
 builder.Services.AddPersistanceServices();
@@ -18,9 +28,84 @@ builder.Services.AddInfrastructureService();
 //Aplication
 builder.Services.AddAutoMapper(typeof(MyProfile));
 
+//Presentation
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+}).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new()
+    {
+        ValidateAudience = true,//tokunumuzu kim/hansi origin islede biler
+        ValidateIssuer = true, //tokunu kim palylayir/yaradir
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true, //tokenin ozel keyi
+
+        ValidAudience = builder.Configuration["Token:Audience"],
+
+        ValidIssuer = builder.Configuration["Token:Issuer"],
+
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Token:SecurityKey"])),
+
+        //tiken omru qeder islemesi ucun
+        LifetimeValidator = (notBefore, expires, securityToken, validationParameters) => expires != null ? expires > DateTime.UtcNow : false,
+
+        NameClaimType = ClaimTypes.Name,
+        RoleClaimType = ClaimTypes.Role,
+    };
+});
+//bunun asagida midilwarede yazmisam(app.Use - bu Proqram.cs da) bu da requestlerin logu ucun lazim
+builder.Services.AddHttpLogging(logging =>
+{
+    logging.LoggingFields = HttpLoggingFields.All;
+    logging.RequestHeaders.Add("sec-ch-ua");
+    logging.MediaTypeOptions.AddText("application/javascript");
+    logging.RequestBodyLogLimit = 4096;
+    logging.ResponseBodyLogLimit = 4096;
+});
+
+builder.Services.AddHttpContextAccessor(); //context gormek ucun http
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(swagger =>
+{
+    //This is to generate the Default UI of Swagger Documentation  
+    swagger.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = "Restaurant Final API",
+        Description = "ASP.NET Core 6 Web API"
+    });
+    // To Enable authorization using Swagger (JWT)  
+    swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme."
+    });
+    swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                          new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new string[] {}
+
+                    }
+                });
+});
 
 var app = builder.Build();
 
@@ -33,6 +118,9 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
